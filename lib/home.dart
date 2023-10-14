6a5379e7ac403b52629ae401b195f8b27fdd08b6
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-10-14 11:41:11 +0700

    creating the flexible template and importing image

--- lib/home.dart
+++ lib/home.dart
@@ -16,12 +16,41 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Color(0xFF92A8D1),
       ),
       body: Container(
+        height: double.infinity,
+        width: double.infinity,
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
             colors: [Color(0xFFB0E0E6), Color(0xFFFFF0E1)],
           ),
+        ),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Flexible(
+              flex: 2,
+              child: Container(
+                child: Text("HELLO 1"),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("HELLO 2.1"),
+                  Text("HELLO 2.2"),
+                  Text("HELLO 2.3"),
+                  Text("HELLO 2.4"),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 4,
+              child: Text("HELLO 3"),
+            ),
+          ],
         ),
       ),
     );
