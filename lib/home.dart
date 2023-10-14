9f0ec87bd49d82de370dd8d2e540e4e721947545
gabrielabernice <gbernice@student.ciputra.ac.id>
2023-10-14 10:25:03 +0700

    making the app bar and gradient color background

--- lib/home.dart
+++ lib/home.dart
@@ -4,12 +4,26 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  State <HomePage> createState() =>  _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class  _HomePageState extends State <HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mission 1"),
+        backgroundColor: Color(0xFF92A8D1),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [Color(0xFFB0E0E6), Color(0xFFFFF0E1)],
+          ),
+        ),
+      ),
+    );
   }
 }
